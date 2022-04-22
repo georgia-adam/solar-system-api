b7ceb35f29703e3850f7f4d98210c09ae3b79b4c
Liliana <liliana.m.parra@gmail.com>
2022-04-22 14:02:49 -0400

    Added Planets class and planets endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
+    from .routes import bp
+    app.register_blueprint(bp)
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, life):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.life = life
+
+
+planets = [
+    Planet(1, "Earth", "Best planet", True),
+    Planet(2, "Saturn", "Got a ring on it", False),
+    Planet(3, "Mars", "We want to go there", False)
+]
+
+bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+@bp.route("", methods=["GET"])
+def get_planets():
+    planets_list = []
+    
+    for planet in planets:
+        planets_list.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "life": planet.life
+        })
+    
+    return jsonify(planets_list)
