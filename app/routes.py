bb3c58a398cba1a5e774ce42dfdaab94ea93902f
Georgia Adam <georgiaeadam@gmail.com>
2022-04-25 15:04:17 -0400

    creating get_one_planet endpoint, make a validate planet helper function, make a Planet instance method to_dict

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, life):
@@ -6,6 +6,14 @@ class Planet:
         self.name = name
         self.description = description
         self.life = life
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            name=self.name,
+            description=self.description,
+            life=self.life
+        )
 
 
 planets = [
@@ -16,16 +24,30 @@ planets = [
 
 bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
+
+def validate_planet(id):
+    try:
+        id = int(id)
+    except:
+        abort(make_response({"message":f"planet {id} invalid"}, 400))
+
+    for planet in planets:
+        if planet.id == id:
+            return planet
+
+    abort(make_response({"message":f"planet {id} not found"}, 404))
+
+
 @bp.route("", methods=["GET"])
 def get_planets():
     planets_list = []
     
     for planet in planets:
-        planets_list.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "life": planet.life
-        })
+        planets_list.append(planet.to_dict())
     
     return jsonify(planets_list)
+
+@bp.route("/<id>", methods=["GET"])
+def get_one_planet(id):
+    planet = validate_planet(id)
+    return planet.to_dict()
